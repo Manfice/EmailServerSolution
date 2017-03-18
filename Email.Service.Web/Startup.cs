30f752fdcee6db5ac26e09e89d6140d2b1b89e1f
Studio Solutions Einstain <Studio Solutions Einstain>
2017-03-18 23:45:01 +0300

    Add project files.

---
+++ Email.Service.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Email.Service.Web.Startup))]
+namespace Email.Service.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
